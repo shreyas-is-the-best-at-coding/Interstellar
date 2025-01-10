13d2888a28a55822ce3f9d9cda1d612536009784
shreyas-is-the-best-at-coding <146784477+shreyas-is-the-best-at-coding@users.noreply.github.com>
2025-01-09 19:04:13 -0600

    Update home.js

--- static/assets/js/home.js
+++ static/assets/js/home.js
@@ -174,14 +174,7 @@ document.addEventListener("DOMContentLoaded", event => {
 const SplashT = [
   "Over 8 Million Users since 2023",
   "Fastest growing proxy server",
-  "Made by xBubbo",
-  "Check out discord.gg/interstellar :)",
-  "Thanks for using the site",
-  "Follow us on Tiktok (@useinterstellar)",
-  "Subscribe to us on YouTube (@unblocking)",
-  "Subscribe to my Youtube (@xbubbo)",
-  "Check out the settings page",
-  "Check out our Patreon (https://www.patreon.com/gointerstellar)",
+  "Best and Fastest proxy for cheap",
 ];
 
 let SplashI = Math.floor(Math.random() * SplashT.length);
@@ -199,18 +192,15 @@ SplashE.addEventListener("click", US);
 function getRandomUrl() {
   const randomUrls = [
     "https://kahoot.it",
-    "https://classroom.google.com",
+    "https://gimkit.com",
     "https://drive.google.com",
     "https://google.com",
     "https://docs.google.com",
     "https://slides.google.com",
     "https://www.nasa.gov",
     "https://blooket.com",
-    "https://clever.com",
-    "https://edpuzzle.com",
     "https://khanacademy.org",
     "https://wikipedia.org",
-    "https://dictionary.com",
   ];
   return randomUrls[randRange(0, randomUrls.length)];
 }
