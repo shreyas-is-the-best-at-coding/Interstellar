27c30fbe38b3536fc4370eef86bf8c4540c5797e
shreyas-is-the-best-at-coding <146784477+shreyas-is-the-best-at-coding@users.noreply.github.com>
2025-01-22 15:31:40 -0600

    Update config.js

--- config.js
+++ config.js
@@ -2,27 +2,7 @@ const config = {
   challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    interstellar: "password",
-    user1: "e)w/S9KE",
-    user2: "hm-z7MQs",
-    user3: "Qx^2)En(",
-    user4: "n2s&]zYm",
-    user5: "Q:9>rN_[",
-    user6: "Q9)8+y}/",
-    user7: "F,2a*=5^",
-    user8: "xuX6&K=8",
-    user9: "H>d2^Lkp",
-    user10: "sW+da8Dr",
-    user11: "SC9Bs~jx",
-    user12: "X[6p`hcB",
-    user13: "xC?h6KY=",
-    user14: "ca`9;bW=",
-    user15: "v9&;z<EB",
-    user16: "C#Qr5*}`",
-    user17: "d4)XC$fB",
-    user18: "B8a6^{dJ",
-    user19: "NvC$9&7w",
-    user20: "p-5hN^6c",
+    username: "password",
   },
 };
 
