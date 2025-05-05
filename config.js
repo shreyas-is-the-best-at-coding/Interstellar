470f2e207956a75cbe5d9531aa57ea8d6872abf0
shreyas-is-the-best-at-coding <146784477+shreyas-is-the-best-at-coding@users.noreply.github.com>
2025-05-05 16:51:08 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,13 @@ const config = {
   challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    hattu: "hattu",
+    vikram: "sigma",
+    sasanth: "aura",
+    druv: "0rizz",
+    hygreev: "cooker",
+    piguy: "3.1415926535",
+    user: "user",
+    file: "sigma.html"
   },
 };
 
